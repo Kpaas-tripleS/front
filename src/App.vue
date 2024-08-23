90a1b82f6671b604b8750552d01381abc2866039
syjdjr <syjdjr@naver.com>
2024-08-23 22:07:42 +0900

    feat: #1 로그인 화면 구현

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <HelloWorld msg="tripleS 프로젝트 시작이지롱"/>
-  <img alt="Vue logo" src="./assets/cat.png">
+  <div id="app">
+    <h1>문해력 (가제)</h1>
+    <img src="./assets/logo.png" alt="로고" class="logo" />
+    <h3 class="description">문해력 키우기(설명 미정)</h3>
+    <LoginComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginComponent from "@/components/LoginComponent.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    LoginComponent,
   }
 }
 </script>
@@ -17,10 +21,14 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.description {
+  font-size: 10px; /* 글자 크기 조정 (원하는 크기로 변경) */
+  color: #666; /* 선택적으로 색상 변경 */
+  margin-bottom: 40px;
 }
 </style>
---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="login">
+    <form @submit.prevent="handleLogin">
+      <input type="text" v-model="username" id="username" placeholder="id 입력하세요" required />
+      <input type="password" v-model="password" id="password" placeholder="password 입력하세요" required />
+      <button type="submit">로그인</button>
+      <button class="sign-up" type="button" @click="handleSignUp">회원이 아니신가요? </button>
+    </form>
+    <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginComponent',
+  data() {
+    return {
+      username: '',
+      password: '',
+      errorMessage: '',
+    };
+  },
+  methods: {
+    async handleLogin() {
+      if (this.username === '' || this.password === '') {
+        this.errorMessage = '모든 필드를 입력하세요.';
+      } else {
+        this.errorMessage = '';
+        alert(`사용자 이름: ${this.username}, 비밀번호: ${this.password}`);
+      }
+    },
+    handleSignUp() {
+      // 회원가입 로직 추가 (예: 회원가입 페이지로 리디렉션)
+      alert("회원가입 페이지로 이동합니다.");
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  max-width: 350px;
+  margin: auto;
+  padding: 20px;
+}
+h1 {
+  color: #333;
+}
+input {
+  margin: 6px 0;
+  padding: 10px;
+  width: 80%;
+  border-radius: 30px;
+  font-size: 10px;
+}
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background: linear-gradient(135deg, #F919C4, #20B7EE);
+  color: white;
+  border: none;
+  border-radius: 30px; /* 둥글게 설정 */
+  cursor: pointer;
+  margin-top: 20px;
+  width: 80%; /* 버튼 너비 조정 */
+}
+button:hover {
+  background-color: #0056b3;
+}
+.error {
+  color: red;
+}
+.sign-up {
+  font-size: 10px;
+  margin-top: 10px; /* 회원가입 버튼과 로그인 버튼 사이의 간격 설정 */
+  background: transparent; /* 배경을 투명하게 설정 */
+  color: #F919C4; /* 텍스트 색상 설정 */
+  text-decoration: underline; /* 밑줄 추가 */
+}
+</style>
